00da5d414ffe8ca469ec1049d719bc8a283c589d
s0582346 <s0582346@htw-berlin.de>
2025-07-05 21:27:13 +0200

    log session me

--- app.api/src/modules/core.ts
+++ app.api/src/modules/core.ts
@@ -19,8 +19,11 @@ export async function coreRoutes(fastify: FastifyInstance) {
 
   fastify.get('/session/me', {
     handler: async (req: FastifyRequest, res: FastifyReply) => {
+      req.log.info({ url: req.url, method: req.method }, '→ /session/me hit')
       const session = await req.server.authenticate(req, res);
 
+      req.log.info({ userId: session?.user.id }, '↳ session loaded')
+      
       return res.status(200).send({
         session,
       });
